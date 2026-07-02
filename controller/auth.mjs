a500d092c133497eadaa3e01e1f22a91ec6b0ff3
이서진 <dltjwls5669@naver.com>
2026-07-02 15:24:48 +0900

    로그인API완료

--- controller/auth.mjs
+++ controller/auth.mjs
@@ -10,7 +10,7 @@ export async function signup(req, res) {
     // 회원 중복 체크
     const found = await authRepository.findByUserid(userid)
     if(found) {
-        return res.status(409).json({ message: `${userid}이(가) 이미 있습니다`})
+        return res.status(409).json({ message: `${userid}이(가) 이미 있습니다` })
     }
     const hashed = bcrypt.hashSync(password, config.bcrypt.saltRounds)
 
@@ -26,7 +26,20 @@ export async function signup(req, res) {
 
 // 로그인
 export async function login(req, res) {
-
+    const { userid, password } = req.body
+    const user = await authRepository.findByUserid(userid)
+    if(user) {
+        const isValidPassword = await bcrypt.compare(password, user.password)
+        if(!isValidPassword) {
+            return res.status(401).json({ message: "아이디 또는 비밀번호 확인" })
+        }
+        const token = await createJwtToken(user.id)
+        res.status(200).json({ token, user })
+    } else {
+        if(!isValidPassword) {
+            return res.status(401).json({ message: "아이디 또는 비밀번호 확인" })
+        }
+    }
 }
 
 // 로그인 유지
